434a6d0c31533336853e47b419d806093ca9bff6
Armin Goodarzi <armingodarzi1380@gmail.com>
2022-01-11 18:27:22 +0330

    Update and rename utopiops-cli.rb to utopiops.rb

---
+++ utopiops.rb
@@ -0,0 +1,14 @@
+class UtopiopsCli < Formula
+  desc "CLI tool for utopiops.com"
+  homepage "https://www.utopiops.com/"
+  url "https://github.com/utopiops/homebrew-utopiops/releases/download/fresh/utopiops-0.0.2.tar.gz"
+  sha256 "8afd60017e3ed33a865ab681c05c0472bd8caddef188d8fde32ceab643b70b24"
+
+  def install
+    bin.install "utopiops"
+  end
+
+  test do
+    system "#{bin}/utopiops", "--version"
+  end
+end
