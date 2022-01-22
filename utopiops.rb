89227fbcaaae7e56e43a69e89bd323fe5249f946
Armin Goodarzi <armingodarzi1380@gmail.com>
2022-01-22 12:56:00 +0330

    Update utopiops.rb

--- utopiops.rb
+++ utopiops.rb
@@ -1,14 +1,14 @@
 class Utopiops < Formula
   desc "CLI tool for utopiops.com"
   homepage "https://www.utopiops.com/"
-  url "https://github.com/utopiops/homebrew-utopiops/releases/download/fresh/utopiops-0.0.3.tar.gz"
-  sha256 "76267f9feff31860ba0d576a09075c1afb7e0e59266ab89037bf4db79301b364"
+  url "https://github.com/utopiops/homebrew-utopiops/releases/download/error/utopiops-mac-0.0.4.tar.gz"
+  sha256 "d6bcf9b8585acdf61b69c70dd80758d98893f1c246f3b197320b6042f2f6a4b0"
 
   def install
     bin.install "utopiops"
   end
 
   test do
-    system "#{bin}/utopiops", "--version"
+    system "#{bin}/utopiops", "--help"
   end
 end
